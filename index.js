01fbeeea3e13681f5d2d3a8af79b087d76ca92af
tloaiza1 <tomasfelipeloaiza@gmail.com>
2019-03-31 22:26:14 -0500

    index verificado y agregado comandos faltantes

--- index.js
+++ index.js
@@ -7,20 +7,28 @@ console.log('Inicia el proceso de registro, si ya estas registrado ingresa');
 let comando=argv._[0];
 switch(comando){
 
-	case 'crear curso':
+	case 'crearCurso':
 		Funciones.crearCurso(argv);
 	break
 	case 'registrar':
 		Funciones.crearUsuario(argv);
 	break
-	
-	case 'Ingresar':
-		Funciones.iniciar(argv);
+	case 'mostrarUsuario':
+		Funciones.listarUsuarios();
 	break
-	
-	case 'Mostrar usuarios':
-		Funciones.listarUsuarios(argv);
+	case 'mostrarCurso':
+		Funciones.listarCursos();
 	break
+	case 'registrarAspirante':
+	Funciones.crearRegistroAspirante(argv);
+	break
+	case 'mostrarRegistro':
+	Funciones.mostrarReg(argv.nombre, argv.nombrecurso);
+	break
+	case 'eliminarCurso':
+	Funciones.eliminarC(argv.nombrecurso);
+	break
+
 	default:
 		console.log('No ingreso un comando existente');
 	break
